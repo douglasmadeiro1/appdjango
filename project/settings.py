0fae0cbf0ff20d11d571e8bfeeefe719b0529861
douglasmadeiro1 <douglas.madfidencio@gmail.com>
2023-06-05 14:52:08 -0300

    segundo commit

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,4 +132,7 @@ STATICFILES_DIR = ('/static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+if os.environ.get("vercel"):
+    STATIC_ROOT = os.path.join(BASE_DIR)
 
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
